ca6487c98f3ac2d880f2b1bd7a20cf3358599db0
“AMIR <amiraj1011@gmail.com>
2021-10-29 20:45:01 -0700

    [ amir UC3 for creating Queue by using Enqueue  ]

--- Program.cs
+++ Program.cs
@@ -1,24 +1,20 @@
 ﻿using System;
 
-namespace StackLinkedList
+namespace QueueLinkedList
 {
     class Program
     {
         static void Main(string[] args)
         {
             //creating object stack class
-            StackLinkedList list = new StackLinkedList();
-            //calling push method
-            list.Push(70);
-            list.Push(30);
-            list.Push(56);
+            QueueLinkedList queue = new QueueLinkedList();
+            //calling Enquue method
+            queue.Enqueve(56);
+            queue.Enqueve(30);
+            queue.Enqueve(70);
             //calling display method
-            list.Display();
-            //calling IsEmpty method
-            list.IsEmpty();
-            list.Display();
-            
-            Console.ReadLine();
+            queue.Display();
+             Console.ReadLine();
 
         }
     }
